b89712189451654102bb95657e2a8d18d4d54ef0
Ana Batres <batres.anaa@gmail.com>
2024-06-12 11:38:18 +0200

    grid

--- grid/src/components/Grid.vue
+++ grid/src/components/Grid.vue
@@ -1,108 +1,68 @@
 <template>
-  <button @click="addWidget">Añadir widget</button>
-  <button @click="removeItem">Eliminar widget</button>
-  <div class="grid-stack">
-    <slot></slot>
+  <div>
+    <button @click="addWidget">Añadir widget</button>
+    <div class="grid-stack"></div>
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-import { GridStack } from 'gridstack';
-import "gridstack/dist/gridstack.min.css";
-import "gridstack/dist/gridstack-extra.min.css";
-
+import { defineComponent } from 'vue'; 
+import { GridStack } from 'gridstack'; 
+import "gridstack/dist/gridstack.min.css"; 
+import "gridstack/dist/gridstack-extra.min.css"; 
+import GridstackItem from "./GrisStackItem.vue"; 
 
 export default defineComponent({
-  name: "Grid",
+  name: "Grid", 
+  components: {
+    GridstackItem, 
+  },
   props: {
     cellHeight: {
       type: String,
-      default: "3.8rem",
+      default: "3.8rem", 
     },
     static: {
       type: Boolean,
-      default: false,
+      default: false, 
     },
     float: {
       type: Boolean,
-      default: false,
-    },
-    save: {
-      type: Boolean,
-      default: false,
-    },
-    width: {
-      type: Number,
-      default: undefined,
-    },
-    height: {
-      type: Number,
-      default: undefined,
-    },
-    widgettype: {
-      type: String,
-    },
-    zone: {
-      type: String,
+      default: false, 
     },
   },
   data() {
     return {
-      columns: "12",
       gridOptions: {
-        resize: true,
-        column: this.columns,
-        cellHeight: this.cellHeight,
-        animate: true,
-        float: this.float,
-        acceptWidgets: true,
-        staticGrid: this.static,
-        compact: true,
+        resize: true, 
+        cellHeight: this.cellHeight, 
+        float: this.float, 
+        staticGrid: this.static, 
       },
       grid: null,
-      saveGrid: this.save,
+      widget: null, 
     };
-  },
-  watch: {
-    save() {
-      this.saveGridMethod();
-      this.saveGrid = false;
-    },
   },
   provide() {
     return {
-      masterLayout: this,
-    }
+      masterLayout: this, // Le da el componente padre a los hijos
+    };
   },
   methods: {
-    //emite el evento update:layout con un valor (this.layout).
-    emitLayout() {
-      this.$emit("update:layout", this.layout);
-    },
     addWidget() {
-      const data = {
-        width: this.width,
-        height: this.height,
-        widgettype: "Tabla",
-        zone: this.zone,
+      this.widget = {
+        typewidget: "Tabla",
       };
-      this.$emit("addWidget", data);
     },
-    removeItem(id) {
-      const index = this.grid.engine.nodes.findIndex((n) => n.id == id);
-      this.grid.removeWidget(this.grid.engine.nodes[index].el, true, true);
-    },
-    //inicializa grid y añade las opciones
     loadGrid() {
       this.grid = GridStack.init(this.gridOptions, '.grid-stack');
-    }
+    },
   },
   mounted() {
-    this.loadGrid();
+    this.loadGrid(); 
   }
 });
 </script>
 
-<style></style>
-thislayout=
+<style>
+</style>
--- grid/src/components/GrisStackItem.vue
+++ grid/src/components/GrisStackItem.vue
@@ -1,143 +1,35 @@
 <template>
-  <div
-    v-if="!editMode"
-    :class="{
-      'grid-stack-item': loaded, //Añade la clase grid-stack-item al div si loaded es true.
-    }"
-  >
-    <div
-      :class="{
-        'grid-stack-item-content': loaded,
-      }"
-    >
-    <!-- :is indica que componente debe ser renderizado dinámicamente. 
-    :data pasa datos al componente dinámicamente.-->
-      <component
-        :is="typewidget"  
-        :data="widgetData"
-        :widgetConfiguration="widgetsConfiguration"
-        @changed="emitChange"
-      ></component>
-    </div>
-  </div>
-  <div
-    v-else
-    :class="{
-      'grid-stack-item': loaded
-    }"
-  >
-    <div
-      :class="{
-        'grid-stack-item-content': loaded,
-      }"
-    >
-      <div class="btns_widgets">
-        <button class="btn_info" @click="enableSettingsChart" v-if="configOption">
-          <i class="bx bx-info-circle"></i>
-        </button>
-        <button
-          v-if="configOption"
-          class="bx bxs-cog conf_btn"
-          ref="configButtons"
-          @click="enableConfigWidget"
-        ></button>
-        <button class="removeItem" @click="remove">
-          <i class="bx bx-x"></i>
-        </button>
-        <button @click="enableSettingsChart" class="bx bx-arrow-back btn_conf_back" />
-      </div>
-      <component
-        :is="typewidget"
-        :data="widgetData"
-        :widgetConfiguration="widgetsConfiguration"
-        @changed="emitChange"
-      ></component>
-    </div>
+  <div class="grid-stack-item-content">
+    <component :is="masterLayout.widget.typewidget" :data="masterLayout.widget.data"
+      :widgetConfiguration="masterLayout.widget.configuration" @changed="emitChange"></component>
   </div>
 </template>
 
 <script>
-import Vue from "vue";
-import Tabla from "@/components/Tabla.vue";
+import { defineComponent } from 'vue';
+import Tabla from "./Tabla.vue";
 
-export default Vue.extend({
+export default defineComponent({
   name: "GridstackItem",
   components: {
     Tabla,
   },
-  props: {
-    id: {
-      type: Number,
-      required: true,
-    },
-    typewidget: {
-      type: String,
-      required: true,
-    },
-    widgetData: {
-      type: Object,
-      required: true,
-    },
-    widgetsConfiguration: {
-      type: Object,
-      required: true,
-    },
-    editMode: {
-      type: Boolean,
-      default: false,
-    },
-    configOption: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  data() {
-    return {
-      loaded: false,
-      enableSettings: false,
-    };
-  },
   inject: {
     masterLayout: {
-      default: null,
+      default: null, // Inyección del componente padre
     },
   },
   methods: {
-    enableConfigWidget() {
-      this.$emit("enableSettings", this.id, this.typewidget);
-    },
-    enableSettingsChart() {
-      this.enableSettings = !this.enableSettings;
-    },
-    remove() {
-      this.masterLayout.grid.removeWidget(this.$el);
-      this.$emit("removed", this.id);
-    },
     emitChange(data) {
       this.$emit("changed", data);
     },
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.loaded = true;
-      this.masterLayout.grid.makeWidget(this.$el);
-    });
-  },
+    mounted() {
+      this.$nextTick(() => {
+        this.masterLayout.grid.makeWidget(this.$el);
+      });
+    }
+  }   
 });
 </script>
 
-<style>
-.grid-stack-item {
-  perspective: 1800px;
-  z-index: 99;
-}
-
-.grid-stack-item-content {
-  box-shadow: var(--box-shadow-cards);
-  transition: all 0.8s linear;
-  background: #ffffff;
-  transform-style: preserve-3d;
-  border-radius: 8px;
-  padding: 0;
-}
-</style>
+<style></style>
